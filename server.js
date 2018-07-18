6548d3593e08a7a854dbbefed19df60ac28f2082
Matt Fewer <mattfewer@hotmail.com>
2018-07-18 00:27:51 -0400

    boilerplate post route

--- server.js
+++ server.js
@@ -4,15 +4,18 @@ const bodyParser = require('body-parser')
 const db = require('./db')
 
 app.use('/public', express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', async (req, res) => {
     db.query('SELECT * FROM orders', (err, results) => {
         if (err) console.log(err)
 
-        console.log(results.rows);
-
         res.sendFile(`${__dirname}/public/index.html`)
     })
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body)
 })
 
 app.listen(process.env.PORT || 8080, () => console.log('Listening...'))
